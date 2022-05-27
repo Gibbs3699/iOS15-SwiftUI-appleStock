0ab092b34e352384d4a864dbe2c31fef9fb75eab
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-05-27 21:27:30 +0700

    Initial Commit

---
+++ Apple Stock/Apple_StockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_StockApp.swift
+//  Apple Stock
+//
+//  Created by TheGIZzz on 27/5/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Apple_StockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
